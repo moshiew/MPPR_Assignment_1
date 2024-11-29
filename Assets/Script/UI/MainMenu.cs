8027aad4b260afd6e07e0d4b8f17c047bac4c538
Teo Kai Xiang <114890789+moshiew@users.noreply.github.com>
2024-11-29 17:49:20 +0800

    Update MainMenu
    - Updated Information for HowToPlay
    - Added Ease In when closing panels

--- Assets/Script/UI/MainMenu.cs
+++ Assets/Script/UI/MainMenu.cs
@@ -45,15 +45,61 @@ public class MainMenu : MonoBehaviour
         Application.Quit();
     }
 
-    public void ClosePanel()
+    public void ClosePanel(int panelIndex)
     {
         // Close panel for the current panel
-        foreach (var panel in menuPanels)
-        {
-            panel.gameObject.SetActive(false);
-        }
+        StartCoroutine(ClosePanelEase(panelIndex));
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="panelIndex"></param>
+    /// <returns></returns>
+    private IEnumerator ClosePanelEase(int panelIndex)
+    {
+        // Reset time
+        float time = 0f;
+
+        Transform targetPanel = menuPanels[panelIndex];
+
+        targetPanel.localScale = Vector3.one;
+
+        // Scale from 100% to 0% 
+        Vector3 startScale = Vector3.one;
+        Vector3 endScale = Vector3.zero;
+
+        while (time < transitionDuration)
+        {
+            time += Time.deltaTime;
+            float t = time / transitionDuration;
+
+            // Quadratic Ease in function
+            bezierCurve.EaseIn(t);
+
+            //Calculate the scale using quadratic ease in function
+            float currentScaleX = startScale.x + (endScale.x - startScale.x) * t;
+            float currentScaleY = startScale.y + (endScale.y - startScale.y) * t;
+            float currentScaleZ = startScale.z + (endScale.z - startScale.z) * t;
+
+            // Set panel scale to new calculated values
+            targetPanel.localScale = new Vector3(currentScaleX, currentScaleY, currentScaleZ);
+
+            yield return null;
+        }
+
+        // Ensure the final scale is exactly as expected
+        targetPanel.localScale = endScale;
+
+        // Disable panel
+        targetPanel.gameObject.SetActive(false);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="panelIndex"></param>
+    /// <returns></returns>
     private IEnumerator TogglePanel(int panelIndex)
     {
         // Reset time
